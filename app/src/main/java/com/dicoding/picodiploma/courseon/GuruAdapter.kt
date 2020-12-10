fc6f3be2829ad9b93aa41c8aed0619516f4cbe4a
hilmysf <hilmy.barca@gmail.com>
2020-12-11 00:23:08 +0700

    semua jago

--- app/src/main/java/com/dicoding/picodiploma/courseon/GuruAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/courseon/GuruAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -20,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_konfirmasi_tambah_jadwal.view.*
+import kotlinx.android.synthetic.main.fragment_pesan_offline.view.*
 import kotlinx.android.synthetic.main.fragment_pesan_online.view.*
 import kotlinx.android.synthetic.main.item_cardview_guru.*
 import kotlinx.android.synthetic.main.item_cardview_guru.view.*
@@ -67,26 +69,68 @@ class GuruAdapter(
         holder.waktu.text = "Waktu: " + model.waktu
 
         holder.btnOnline.setOnClickListener {
-            showDialog(model)
+            showDialogOnline(model)
         }
         holder.btnOffline.setOnClickListener {
-            showDialog(model)
+            showDialogOffline(model)
         }
     }
 
-    private fun showDialog(pemesanan: GuruModel) {
+    private fun showDialogOffline(pemesanan: GuruModel) {
 
-        builder.setTitle("Konfirmasi Data")
+        builder.setTitle("Konfirmasi Pemesanan")
+        val view = inflater.inflate(R.layout.fragment_pesan_offline, null)
+
+        val etNamaGuru = view.findViewById<TextView>(R.id.tv_nama_guru_pesanan_offline)
+        val etTanggal = view.findViewById<TextView>(R.id.tv_tanggal_offline)
+        val etWaktu = view.findViewById<TextView>(R.id.tv_waktu_offline)
+        val etAlamat = view.findViewById<EditText>(R.id.edt_alamat)
+
+        etNamaGuru.text = pemesanan.nama
+        etTanggal.text = pemesanan.tanggal
+        etWaktu.text = pemesanan.waktu
+        view.tv_matpel_offline.text = matpel
+
+        builder.setView(view)
+        builder.setPositiveButton("SIMPAN") { dialogInterface, id ->
+//            saveData()
+            val user = mAuth.currentUser
+            val db = Firebase.firestore
+            val pemesanan = hashMapOf(
+                "Tipe Pemesanan" to "Offline",
+                "alamat_pemesanan" to etAlamat.text.toString(),
+                "matpel" to matpel,
+                "nama" to etNamaGuru.text.toString(),
+                "nama_pemesan" to user?.displayName.toString(),
+                "status" to "Menunggu Konfirmasi",
+                "tanggal" to etTanggal.text.toString(),
+                "waktu" to etWaktu.text.toString()
+            )
+
+            db.collection("pemesanan")
+                .add(pemesanan)
+            Toast.makeText(mCtx, "Berhasil menambahkan jadwal", Toast.LENGTH_LONG).show()
+//            startActivity(view.context,Intent(mCtx,MainActivityGuru::class.java))
+            view.context.startActivity(Intent(mCtx, LihatLogActivity::class.java))
+        }
+        builder.setNegativeButton("Batalkan") { dialogInterface, id ->
+        }
+        val alert: AlertDialog = builder.create()
+        alert.show()
+    }
+    private fun showDialogOnline(pemesanan: GuruModel) {
+
+        builder.setTitle("Konfirmasi Pemesanan")
         val view = inflater.inflate(R.layout.fragment_pesan_online, null)
 
         val etNamaGuru = view.findViewById<TextView>(R.id.tv_nama_guru_pesanan_online)
         val etTanggal = view.findViewById<TextView>(R.id.tv_tanggal_online)
         val etWaktu = view.findViewById<TextView>(R.id.tv_waktu_online)
 
+        etNamaGuru.text = pemesanan.nama
         etTanggal.text = pemesanan.tanggal
         etWaktu.text = pemesanan.waktu
-        view.tv_matpel_online.text = matpel
-        view.tv_jenis_pemesanan_online.text = "Online"
+//        view.tv_matpel_online.text = matpel
         builder.setView(view)
         builder.setPositiveButton("SIMPAN") { dialogInterface, id ->
 //            saveData()
@@ -114,6 +158,5 @@ class GuruAdapter(
         val alert: AlertDialog = builder.create()
         alert.show()
     }
-
 
 }
